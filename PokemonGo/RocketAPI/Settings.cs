8d2c0c964c4d2f51785579b44a6c329fcb2d86a9
ShiftCode <simon@shiftcode.pw>
2016-07-20 23:57:46 +0200

    Updated dependencies

--- PokemonGo/RocketAPI/Settings.cs
+++ PokemonGo/RocketAPI/Settings.cs
@@ -9,15 +9,15 @@ namespace PokemonGo.RocketAPI
     public static class Settings
     {
         //Fetch these settings from intercepting the /auth call in headers and body (only needed for google auth)
-        public const bool UsePTC = true; // Change this to true if you want to use Pokemon Trainer Club account.
+        public const bool UsePTC = false; // Change this to true if you want to use Pokemon Trainer Club account.
         public const string PtcUsername = "username"; // Change this to your pokemon trainer club account username
         public const string PtcPassword = "password"; // Change this to your pokemon trainer club account password
         public const string DeviceId = "SM-G925F";
         public const string Email = "fake@gmail.com";
         public const string ClientSig = "fake";
         public const string LongDurationToken = "fakeid";
-        public const double DefaultLatitude = 0;
-        public const double DefaultLongitude = 0;
+        public const double DefaultLatitude = 45.030152;
+        public const double DefaultLongitude = -93.31931;
 
     }
 }
